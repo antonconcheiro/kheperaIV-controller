8829813dfee8ebb2688c6451754ecef8d6a021a5
Anton <antonconcheirofdez@gmail.com>
2022-04-16 21:45:57 +0200

    Corregido errores mapeado

--- odometria.py
+++ odometria.py
@@ -38,7 +38,7 @@ direction = 0
 
 # 0-No obstacle, 1-No visited, 2-Visited, 3-Obstacle
 def mark_pos(pos, ir):
-    if ir > 200:
+    if ir > 190:
         map[pos] = 3
     elif map[pos] != 2:
         map[pos] = 0
@@ -47,43 +47,48 @@ def mark_current(pos):
     map[pos] = 2
     
 def mark_near(pos, direction, ir):
-    if direction == 0:
+    if direction%4 == 0:
         mark_pos((pos[0],pos[1]+1), ir[0]) # Left
         mark_pos((pos[0],pos[1]-1), ir[1]) # Right
         mark_pos((pos[0]+1,pos[1]), ir[2]) # Fwd
-    elif direction == 1:
+    elif direction%4 == 1:
         mark_pos((pos[0]-1,pos[1]), ir[0]) # Left
         mark_pos((pos[0]+1,pos[1]), ir[1]) # Right
         mark_pos((pos[0],pos[1]+1), ir[2]) # Fwd
-    elif direction == 2:
+    elif direction%4 == 2:
         mark_pos((pos[0],pos[1]-1), ir[0]) # Left
         mark_pos((pos[0],pos[1]+1), ir[1]) # Right
         mark_pos((pos[0]-1,pos[1]), ir[2]) # Fwd
-    elif direction == 3:
+    elif direction%4 == 3:
+        print(pos)
+        print((pos[0]+1,pos[1]))
+        print((pos[0]-1,pos[1]))
+        print((pos[0],pos[1]-1))
         mark_pos((pos[0]+1,pos[1]), ir[0]) # Left
         mark_pos((pos[0]-1,pos[1]), ir[1]) # Right
         mark_pos((pos[0],pos[1]-1), ir[2]) # Fwd
+    print(map)
         
 def decide_dir(pos, dir):
-    if direction == 0:
+    if dir%4 == 0:
         array_directions = [
             map[(pos[0],pos[1]+1)],
             map[(pos[0]+1,pos[1])],
             map[(pos[0],pos[1]-1)]
         ] # Left, fwd, right
-    elif direction == 1:
+    elif dir%4 == 1:
         array_directions = [
             map[(pos[0]-1,pos[1])],
             map[(pos[0],pos[1]+1)],
             map[(pos[0]+1,pos[1])]
         ] # Left, fwd, right
-    elif direction == 2:
+    elif dir%4 == 2:
         array_directions = [
             map[(pos[0],pos[1]-1)],
             map[(pos[0]-1,pos[1])],
             map[(pos[0],pos[1]+1)]
         ] # Left, fwd, right
-    elif direction == 3:
+    elif dir%4 == 3:
         array_directions = [
             map[(pos[0]+1,pos[1])],
             map[(pos[0],pos[1]-1)],
@@ -100,9 +105,6 @@ def stop_robot():
     rightWheel.setVelocity(0)
     
 def move_fwd(current_pos_map, direction):
-    #print(map)
-    #print(current_pos_map)
-    #print(direction)
     if direction == 0:
         current_pos_map = (current_pos_map[0] + 1, current_pos_map[1])
     elif direction == 1:
@@ -154,8 +156,6 @@ movement = 0
 
 while robot.step(TIME_STEP) != -1:
     current_pos = encoderL.getValue()
-    #print(f"current: {current_pos}, initial: {initial_pos}")
-    #print(current_pos - initial_pos)
     if (movement == 0 and current_pos - initial_pos >= FORWARD_ONE):
         stopped = 0
     elif (movement == 1 and initial_pos - current_pos >= TURN_VAL -0.001):
@@ -166,10 +166,7 @@ while robot.step(TIME_STEP) != -1:
         mark_current(current_pos_map)
         ir_values = measure_ir()
         mark_near(current_pos_map, direction, ir_values)
-        print(map)
-        print(current_pos_map)
         next_dir = decide_dir(current_pos_map, direction) # 0-Left, 1-Fwd, 2-Right, -1-Back
-        print(next_dir)
         if(next_dir == 0 and movement != 1):
             movement = 1
             stopped = 1
@@ -179,7 +176,6 @@ while robot.step(TIME_STEP) != -1:
             movement = 0
             stopped = 1
             initial_pos = encoderL.getValue()
-            #print(direction)
             current_pos_map = move_fwd(current_pos_map, direction)
         elif(next_dir == 2):
             movement = 2
